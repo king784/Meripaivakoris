86b77e2c1149a7cf4d43a281f5643aba25f73ad6
king784 <teemu.turku@pp2.inet.fi>
2019-07-25 14:25:33 +0300

    jee

---
+++ JS/SaveExcel.js
@@ -0,0 +1,19 @@
+function start()
+{
+    var data = [{"Joukkue":"KTP-basket", "Pisteet":"2"},
+    {"Joukkue":"Karhut", "Pisteet":"1"},
+    {"Joukkue":"Kakka", "Pisteet":"69"}
+    ];
+
+    if(typeof XLSX == 'undefined')
+    {
+        XLSX = require('xlsx');
+    }
+
+    var ws = XLSX.utils.json_to_sheet(data);
+
+    var wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "Matsit");
+
+    XLSX.writeFile(wb, "JepaJee.xlsx");
+}
